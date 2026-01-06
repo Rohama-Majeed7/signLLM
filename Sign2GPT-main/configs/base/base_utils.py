54619d22c2b5630f141f51a16c2811759821dc90
Rohama Majeed <rohamamajeed3@gmail.com>
2026-01-06 14:16:37 +0500

    file path changes

--- Sign2GPT-main/configs/base/base_utils.py
+++ Sign2GPT-main/configs/base/base_utils.py
@@ -2,10 +2,10 @@ from pathlib import Path
 
 
 def get_checkpoint_path(base_name, name):
-    ckpt_path = "<YOUR_CHECKPOINT_DIRECTORY>"
+    ckpt_path = "/kaggle/working/signLLM/Sign2GPT-main/checkpoints"
     return ckpt_path
 
 
 def get_lmdb_path():
-    lmdb_path = "<YOUR_LMDB_DIRECTORY>"
+    lmdb_path = "/kaggle/input/phoenixweather2014t-3rd-attempt"
     return lmdb_path
