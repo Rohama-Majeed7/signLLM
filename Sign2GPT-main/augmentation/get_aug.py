747f6542ca1147d8329b136e397c8e291a23ad38
Rohama Majeed <rohamamajeed3@gmail.com>
2026-01-06 16:20:01 +0500

    aug

--- Sign2GPT-main/augmentation/get_aug.py
+++ Sign2GPT-main/augmentation/get_aug.py
@@ -2,15 +2,15 @@ import importlib
 
 def get_aug(aug_name, aug_params=None):
     if aug_name is not None:
-        # Make a mutable dict
+        # Convert frozen ConfigDict to normal dict
         if aug_params is None:
             aug_params = {}
         else:
-            aug_params = dict(aug_params)  # convert ConfigDict to normal dict
+            aug_params = dict(aug_params)  # make mutable copy
 
-        # Ensure required parameters exist
-        if "size" not in aug_params:  # Albumentations new version
-            aug_params["size"] = (224, 224)
+        # Albumentations RandomResizedCrop in newer versions requires 'size'
+        if "size" not in aug_params:
+            aug_params["size"] = (224, 224)  # replace height + width
 
         mod = importlib.import_module(aug_name, package=None)
         return mod.Transformation(**aug_params)
